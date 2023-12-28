408ac687f069183200d9317f711a33846a9aef68
lmlenis <llenis73@gmail.com>
2023-12-27 21:10:02 -0500

    cambio en app

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -8,7 +8,7 @@ import Router from 'src/routes/sections';
 import ThemeProvider from 'src/theme';
 import axios from 'axios';
 
-axios.defaults.baseURL= 'http://localhost:3001';
+axios.defaults.baseURL= 'https://etnia-ecommerce-production.up.railway.app/';
 // ----------------------------------------------------------------------
 
 export default function App() {
